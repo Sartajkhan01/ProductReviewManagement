fd1c1db100000b43418098db3634f4eeceb94951
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-02-22 22:32:47 +0530

    [Sartaj] added skip top 5 record program

--- LinqProblems/Program.cs
+++ LinqProblems/Program.cs
@@ -12,7 +12,8 @@ namespace LinqProblems
                 Console.WriteLine("Welcome To Product Review Management System\n");
                 List<ProductReview> list = ProductReviewManagement.AddProductsReview();
                 Console.WriteLine("Choose Option or press 0 for exit\n1:Add review to the list\n2:Top 3 high Rated product\n" +
-                    "3:Products with Rating greater than 3\n4:Count of products for each ProductID\n5:Only ProductID with Review");
+                    "3:Products with Rating greater than 3\n4:Count of products for each ProductID\n5:Only ProductID with Review\n" +
+                    "6:Skipping top 5 records and displaying remaining records");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -33,6 +34,9 @@ namespace LinqProblems
                         break;
                     case 5:
                         ProductReviewManagement.RetrieveProductIDWithReview(list);
+                        break;
+                    case 6:
+                        ProductReviewManagement.SkipTop5Records(list);
                         break;
                 }
                 Console.ReadLine();
