8efd35a13f627a0c3acea86d99fa8b41006125e6
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-02-22 22:16:56 +0530

    [Sartaj] added creating list of product program

---
+++ LinqProblems/Program.cs
@@ -0,0 +1,15 @@
+﻿using LinqProblems;
+
+namespace LinqProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Product Review Management System");
+            List<ProductReview> list = ProductReviewManagement.AddProductsReview();
+            ProductReviewManagement.DisplayeProductsReview(list);
+            Console.ReadLine();
+        }
+    }
+}
